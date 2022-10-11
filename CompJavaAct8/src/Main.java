50452d4e30af174bbb0e724dc11333cabc2a260e
Patricio <patoestradac@gmail.com>
2022-10-11 17:52:54 -0500

    Actualizacion excepcion entrada vacia

--- CompJavaAct8/src/Main.java
+++ CompJavaAct8/src/Main.java
@@ -12,7 +12,7 @@ public class Main
         Deck baraja = new Deck();   //Se crea un objeto de tipo Deck que se usará para el programa
 
         String eleccionCompleta;        //Captura el valor de la string elegida completa
-        char eleccionSwit ='0';         //Guarda el primer caracter de la opcion elegida para poder utilizarse en un switch
+        char eleccionSwit='0';         //Guarda el primer caracter de la opcion elegida para poder utilizarse en un switch
         int salirProceso;               //La opcion que guarda el int que dicta si salir del programa o no
         boolean accionValida = false;   //Boolean que permite que las opciones puedan repetirse indefinidamente hasta que el usuario desee salir del programa
 
@@ -25,6 +25,38 @@ public class Main
                 //Despliega el panel de opciones posibles dentro del programa
                 eleccionCompleta = (String) JOptionPane.showInputDialog(null,"Seleccione una opcion:\n\t1) Reordenar baraja\n\t2) Agarrar carta de hasta arriba\n\t3) Agarrar carta aleatoria\n\t4) Pedir mano\n\n\t0) Salir del programa", "MENU ACTIVIDAD 9: Baraja Poker v2.0", JOptionPane.QUESTION_MESSAGE);
                 eleccionSwit = eleccionCompleta.charAt(0);
+
+                switch(eleccionSwit) //Dependiendo de la opción elegida, se ejecuta la acción especifica, esto se movió aquí para poder atender mejor la excepción generada por no ingresar nada en la caja de texto
+                {
+                    case '1':
+                        baraja.Shuffle(false);
+                        accionValida = false;
+                        break;
+
+                    case '2':
+                        baraja.Head();
+                        accionValida = false;
+                        break;
+
+                    case '3':
+                        baraja.Pick();
+                        accionValida = false;
+                        break;
+
+                    case '4':
+                        baraja.Hand();
+                        accionValida = false;
+                        break;
+
+                    case '0':
+                        System.exit(0);
+                        break;
+
+                    default:
+                        JOptionPane.showMessageDialog(null,"Comando no reconocido, vuelva a intentarlo", "ACTIVIDAD 9 Baraja Poker v2.0", JOptionPane.ERROR_MESSAGE);
+                        accionValida = false;
+                        break;
+                }
             }
 
             catch(NullPointerException a) //El usuario seleccionó la opcion de cerrar el mensaje o de cancelar
@@ -43,41 +75,13 @@ public class Main
                 else
                 {
                     accionValida = false;
-                    continue;
                 }
             }
 
-            //Dependiendo de la opción elegida, se ejecuta la acción especifica
-            switch(eleccionSwit)
+            catch(IndexOutOfBoundsException b) //El usuario no ingreso nada y dió aceptar de todas formas
             {
-                case '1':
-                    baraja.Shuffle(false);
-                    accionValida = false;
-                    break;
-
-                case '2':
-                    baraja.Head();
-                    accionValida = false;
-                    break;
-
-                case '3':
-                    baraja.Pick();
-                    accionValida = false;
-                    break;
-
-                case '4':
-                    baraja.Hand();
-                    accionValida = false;
-                    break;
-
-                case '0':
-                    System.exit(0);
-                    break;
-
-                default:
-                    JOptionPane.showMessageDialog(null,"Comando no reconocido, vuelva a intentarlo", "ACTIVIDAD 9 Baraja Poker v2.0", JOptionPane.ERROR_MESSAGE);
-                    accionValida = false;
-                    break;
+                JOptionPane.showMessageDialog(null,"Comando no reconocido, vuelva a intentarlo", "ACTIVIDAD 9 Baraja Poker v2.0", JOptionPane.ERROR_MESSAGE);
+                accionValida = false;
             }
         }
         while(!accionValida);
